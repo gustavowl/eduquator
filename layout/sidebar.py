a905924b9d86e409a06b1646120cd2dd49f75add
gustavowl <gustavoalvesbezerra@gmail.com>
2018-11-25 18:40:45 -0300

    Changes sidebar to class

--- layout/sidebar.py
+++ layout/sidebar.py
@@ -5,35 +5,31 @@ class Sidebar(Frame):
     #widgets
 
     def createWidgets(self):
-        print "qwertyuio"
-        self.var = 73
+        self.fr = ttk.Frame(self, borderwidth=5, relief="sunken",
+                width=320, height=640)
+        self.tools_label = ttk.Label(self.fr, text="Tools")
+        self.button1 = ttk.Button(self.fr, text="1")
+        self.button2 = ttk.Button(self.fr, text="2")
+        self.button3 = ttk.Button(self.fr, text="3")
+        self.button4 = ttk.Button(self.fr, text="4")
+        self.tools_separator = ttk.Separator(self.fr, orient=HORIZONTAL)
+        self.circuit_label = ttk.Label(self.fr, text="Circuit Components")
 
+        self.fr.grid(column=0, row=0)
+        self.tools_label.grid(column=0, row=0, columnspan=2)
+        self.button1.grid(column=0, row=1)
+        self.button2.grid(column=1, row=1)
+        self.button3.grid(column=0, row=2)
+        self.button4.grid(column=1, row=2)
+        self.tools_separator.grid(row=3, columnspan=2, sticky=(W, E))
+        self.circuit_label.grid(row=4, columnspan=2)
 
-content = ttk.Frame(root)
-frame = ttk.Frame(content, borderwidth=5, relief="sunken",
-        width=320, height=640)
-tools_label = ttk.Label(frame, text="Tools")
-button1 = ttk.Button(frame, text="1")
-button2 = ttk.Button(frame, text="2")
-button3 = ttk.Button(frame, text="3")
-button4 = ttk.Button(frame, text="4")
-tools_separator = ttk.Separator(frame, orient=HORIZONTAL)
-circuit_label = ttk.Label(frame, text="Circuit Components")
-
-content.grid(column=0, row=0)
-frame.grid(column=0, row=0, columnspan=2, rowspan=5)
-#widgets
-tools_label.grid(column=0, row=0, columnspan=2)
-button1.grid(column=0, row=1)
-button2.grid(column=1, row=1)
-button3.grid(column=0, row=2)
-button4.grid(column=1, row=2)
-tools_separator.grid(row=3, columnspan=2, sticky=(W, E))
-circuit_label.grid(row=4, columnspan=2)
+        for child in self.fr.winfo_children():
+            child.grid_configure(padx=3, pady=3)
 
     def __init__(self, parent=None):
         Frame.__init__(self, parent)
-        #self.pack()
+        self.pack()
         self.createWidgets()
 
 root = Tk()
@@ -41,31 +37,3 @@ sb = Sidebar(parent=root)
 #print sb.var #works
 sb.mainloop()
 
-"""
-content = ttk.Frame(root)
-frame = ttk.Frame(content, borderwidth=5, relief="sunken",
-        width=320, height=640)
-tools_label = ttk.Label(frame, text="Tools")
-button1 = ttk.Button(frame, text="1")
-button2 = ttk.Button(frame, text="2")
-button3 = ttk.Button(frame, text="3")
-button4 = ttk.Button(frame, text="4")
-tools_separator = ttk.Separator(frame, orient=HORIZONTAL)
-circuit_label = ttk.Label(frame, text="Circuit Components")
-
-content.grid(column=0, row=0)
-frame.grid(column=0, row=0, columnspan=2, rowspan=5)
-#widgets
-tools_label.grid(column=0, row=0, columnspan=2)
-button1.grid(column=0, row=1)
-button2.grid(column=1, row=1)
-button3.grid(column=0, row=2)
-button4.grid(column=1, row=2)
-tools_separator.grid(row=3, columnspan=2, sticky=(W, E))
-circuit_label.grid(row=4, columnspan=2)
-
-for child in frame.winfo_children():
-    child.grid_configure(padx=3, pady=3)
-
-
-root.mainloop()"""
